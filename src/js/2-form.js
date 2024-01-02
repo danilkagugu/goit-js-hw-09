700a6b2ce6090c726c9b0628c5e3e07bd1549316
Danulo <danilyanishevski2001@gmail.com>
2024-01-02 13:09:27 +0200

    correct

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,25 @@
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', onTextareaInput);
+form.addEventListener('input', formData);
 
 populateTextarea();
 
 function onFormSubmit(e) {
   e.preventDefault();
+  const emailValue = form.elements.email.value;
+  const messageValue = form.elements.message.value;
+  if (!emailValue || !messageValue) {
+    alert('Заповніть всі поля!');
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
 }
 
-function onTextareaInput(e) {
+function formData() {
   const formData = {
     email: form.elements.email.value.trim(),
     message: form.elements.message.value.trim(),
